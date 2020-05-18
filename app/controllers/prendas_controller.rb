2b24f3e4eb16f5a1d5c728d98b1e077efb52cf87
Agustín <agustinnicolas_@hotmail.com>
2020-05-18 16:31:52 -0300

    UpdateDestroy

--- app/controllers/prendas_controller.rb
+++ app/controllers/prendas_controller.rb
@@ -10,6 +10,8 @@ class PrendasController < ApplicationController
     
     def destroy
         Prenda.destroy(params[:id].to_i)
+        @prenda.destroy
+        redirect_to "index", notice => "La prenda se elimino"
     end 
 
     def new
@@ -29,10 +31,18 @@ class PrendasController < ApplicationController
     end
     
     def edit
-
+        @prenda = Prenda.find (params [:id].to_i)
     end
 
+    
     def update
+        @prenda = Prenda.find(params [:id].to_i)
 
+        if @prenda.update_attributes(params[:prenda])
+            redirect_to "index", notice => "La prenda se modifico exitosamente"
+
+        else
+            render ":edit"
+        end
     end
 end
