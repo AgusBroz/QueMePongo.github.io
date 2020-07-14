a7dd92868c783152ec1612a49ae8dfc9bf206136
Agustin <agustinbrozarias@gmai.com>
2020-07-14 15:51:11 -0300

    mailer en production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,6 +61,16 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "queMePongoTp_production"
 
   config.action_mailer.perform_caching = false
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "evening-sierra-46449.herokuapp.com",
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: "quemepongotm@gmail.com",
+    password: "quemepongo2k20"
+  }
+  config.action_mailer.default_url_options = { :host => "https://evening-sierra-46449.herokuapp.com/" }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
