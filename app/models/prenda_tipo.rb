a1e7c715b01e80c68bff764c2afc796b26804d1f
Agustin <agustinbrozarias@gmai.com>
2020-07-15 13:00:13 -0300

    precompilacion de assets en produccion

--- app/models/prenda_tipo.rb
+++ app/models/prenda_tipo.rb
@@ -3,5 +3,6 @@ class PrendaTipo < ApplicationRecord
     
     enum nombre: [:gorro, :sombrero, :remera, :buzo, :campera, :jogging, :zapatos, :zapatillas, :ojotas, :sandalias, :chomba, :camisa, :suéter, :polera, :chaleco, :musculosa, 
     :vestido, :pollera, :jean, :short, :bermuda, :tacones, :crocs ]
+    
     enum categoria: [:cabeza, :torso, :piernas, :pies]
 end
